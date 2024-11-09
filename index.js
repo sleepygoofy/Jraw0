493d7a601202e74813e9c6efd33a6d2adfa7a1d4
zyqunix <117040076+zyqunix@users.noreply.github.com>
2024-11-09 17:56:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -118,5 +118,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function copyToClipboard(text) {
     navigator.clipboard.writeText(text);
-    alert("Copied to clipboard!");
+    alert("Address Copied to Clipboard");
 }
